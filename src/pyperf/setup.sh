2f96fde9c58cf9b21b9655f5890e7768de645e38
manishshettym <manishs@berkeley.edu>
2024-09-13 19:02:18 -0700

    feat: setup and extract functions in single script

---
+++ src/pyperf/setup.sh
@@ -0,0 +1,34 @@
+# Script to setup a repository for pyperf deployment
+REPO_URL="https://github.com/r2e-project/r2e.git"
+LOCAL_REPO_PATH="~/buckets/my_repos/r2e"
+EXP_ID="demo"
+MULTIPROCESS=8
+CACHE_BATCH_SIZE=100
+
+# Parse arguments
+while getopts u:p:e:m:c: flag
+do
+    case "${flag}" in
+        u) REPO_URL=${OPTARG};;
+        p) LOCAL_REPO_PATH=${OPTARG};;
+        e) EXP_ID=${OPTARG};;
+        m) MULTIPROCESS=${OPTARG};;
+        c) CACHE_BATCH_SIZE=${OPTARG};;
+    esac
+done
+
+REPO_NAME=$(basename $REPO_URL .git)
+
+# Clone the repository and move it to R2E's workspace
+if [ ! -d ~/buckets/local_repoeval_bucket/repos/$REPO_NAME ]; then
+    git clone $REPO_URL ~/buckets/local_repoeval_bucket/repos/$REPO_NAME
+fi
+
+# Extract functions and methods from the repo
+python src/r2e/r2e/repo_builder/extract_func_methods.py --exp_id $EXP_ID --overwrite_extracted --disable_all_filters
+
+
+# cd to the repo and install it
+cd ~/buckets/local_repoeval_bucket/repos/$REPO_NAME
+uv pip install -r requirements_dev.txt
+uv run setup.py install
