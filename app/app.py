5ea2ecccb7fc4fde310eec2a9c7789ffc03660f2
manishshettym <manishs@berkeley.edu>
2024-09-23 17:58:53 -0700

    feat: add pagination for easier loading

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from fuzzywuzzy import fuzz
 
 from utils import load_data, get_test_status, get_function_name
 
-
 json_file_path = "/home/manishs/buckets/pyperf_bucket/testgen/pylint_out.json"
 data = load_data(json_file_path)
 app = Flask(__name__)
@@ -16,22 +15,14 @@ def index():
     try:
         # Calculate overall stats
         total_functions = len(data)
-        successful_tests = 0
-        failed_tests = 0
-        errors = 0
-
-        for item in data:
-            status = get_test_status(item)
-            if status == "success":
-                successful_tests += 1
-            elif status == "failure":
-                failed_tests += 1
-            else:
-                errors += 1
+        successful_tests = sum(1 for item in data if get_test_status(item) == "success")
+        failed_tests = sum(1 for item in data if get_test_status(item) == "failure")
+        errors = sum(1 for item in data if get_test_status(item) == "error")
 
         # Apply filters
         filter_type = request.args.get("filter", "all")
         remove_tests = request.args.get("remove_tests", "false").lower() == "true"
+        search_query = request.args.get("search", "").lower()
 
         def fid(x):
             return x.get("function_id", x.get("method_id", {})).get("identifier", "")
@@ -51,7 +42,6 @@ def index():
             ]
 
         # Apply search
-        search_query = request.args.get("search", "").lower()
         if search_query:
             filtered_data = sorted(
                 [item for item in filtered_data if search_query in fid(item).lower()],
@@ -59,9 +49,16 @@ def index():
                 reverse=True,
             )
 
+        # Pagination
+        page = int(request.args.get("page", 1))
+        if page < 1 or page > len(filtered_data):
+            page = 1
+
+        current_item = filtered_data[page - 1] if filtered_data else None
+
         return render_template(
             "base.html",
-            data=filtered_data,
+            item=current_item,
             total_functions=total_functions,
             successful_tests=successful_tests,
             failed_tests=failed_tests,
@@ -69,6 +66,8 @@ def index():
             search_query=search_query,
             filter_type=filter_type,
             remove_tests=remove_tests,
+            current_page=page,
+            total_pages=len(filtered_data),
         )
     except json.JSONDecodeError:
         return "Invalid JSON file.", 400
@@ -77,6 +76,6 @@ def index():
 
 
 if __name__ == "__main__":
-    # host = "0.0.0.0"
-    port = 5777
-    app.run(debug=True, port=port)
+    host = "0.0.0.0"
+    port = 5600
+    app.run(debug=True, host=host, port=port)
