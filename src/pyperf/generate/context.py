782080152e435b2c4bcd0c5bb1f60913a8f96440
manishshettym <manishs@berkeley.edu>
2024-10-11 10:35:34 -0700

    chore: cleanup main

--- src/pyperf/generate/context.py
+++
@@ -1,8 +0,0 @@
-from r2e.generators.context.manager import ContextManager
-from r2e.models.context import Context
-
-
-def get_context_wrapper(args) -> Context:
-    """A wrapper over r2e.context.ContextManager.get_context to be used in parallel processing"""
-    context_type, func_meth, max_context_size = args
-    return ContextManager.get_context(context_type, func_meth, max_context_size)
--- src/pyperf/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from pyperf.models.fut import BaseUnderTest, FunctionUnderTest, MethodUnderTest
-from pyperf.models.fut import create_code_under_test
-from pyperf.models.tests import Tests
-from pyperf.models.history import TestHistory
