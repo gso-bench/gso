ec0de39cc83bc4fc5fb91cc9e9588bf6dddd581a
manishshettym <manishs@berkeley.edu>
2024-10-13 15:22:29 -0700

    wip: scaffolding for generator

---
+++ src/pyperf/generate/generate.py
@@ -0,0 +1,13 @@
+class PerfExpGenerator:
+    """Generate performance testing experiments for a given repository API"""
+
+    def __init__(self, repo, base_commit, test_commits, mode):
+        pass
+
+    def generate():
+        """Propose performance test experiments for APIs"""
+        pass
+
+    def execute():
+        """Execute proposed test against a set of test commits"""
+        pass
