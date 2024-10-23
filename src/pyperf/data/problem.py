79da16628b96789456f0773abacb464d09f1345e
manishshettym <manishs@berkeley.edu>
2024-10-22 17:29:11 -0700

    minor: default factory for setup and install commands

--- src/pyperf/data/problem.py
+++ src/pyperf/data/problem.py
@@ -11,23 +11,39 @@ class Problem(BaseModel):
     cloud: str = Field(default="gcp", description="Cloud provider")
     region: str = Field(default="us-central1", description="Cloud region")
     instance_type: str = Field(default="n2-standard-16", description="Instance type")
+    py_version: str = Field(default="3.9", description="Python version")
 
     # commit info
     base_commit: str = Field(..., description="Commit hash for before test")
     target_commit: str = Field(default="main", description="Commit hash for after test")
 
     # commands
-    setup_commands: list[str] = Field(
-        default_factory=list, description="Setup commands to run"
-    )
-    install_commands: list[str] = Field(
-        default_factory=list, description="Install commands to run"
-    )
+    setup_commands: list[str] = Field(init=False, default=[])
+    install_commands: list[str] = Field(init=False, default=[])
 
     chat_messages: list[dict[str, str]] = Field(default=[], description="Chat messages")
     test: str = Field(
         default="if __name__ == '__main__': pass", description="Test code to run"
     )
+
+    def model_post_init(self, __context) -> None:
+        if self.setup_commands == []:
+            self.setup_commands = [
+                "sudo apt-get install -y libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev",
+                "sudo apt-get install -y libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk",
+                "sudo apt-get install -y libharfbuzz-dev libfribidi-dev libxcb1-dev libx11-dev",
+            ]
+
+        if self.install_commands == []:
+            self.install_commands = [
+                f"uv venv --python {self.py_version}",
+                "source .venv/bin/activate",
+                "which python",
+                "python --version",
+                "uv pip install -e .",
+                "uv pip install requests",
+                f"uv pip show {self.repo.repo_name}",
+            ]
 
     # helpers to generate test
 
