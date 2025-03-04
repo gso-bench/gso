6192b4d4bcc9f960d55446b616c06543967efd13
manishshettym <manishs@berkeley.edu>
2025-03-04 10:22:52 -0800

    refactor(config): remove config file and update bucket directory path

--- src/pyperf/constants.py
+++ src/pyperf/constants.py
@@ -3,18 +3,12 @@ from pathlib import Path
 from pathlib import Path
 import yaml
 
-# --------- Configurations ---------
-
-# Load configuration from config.yaml
 current_dir = Path(__file__).parent
-config_path = current_dir / "config.yml"
-with open(config_path, "r") as file:
-    config: dict[str, str] = yaml.safe_load(file)  # type: ignore
 
 # --------- Path Constants ---------
 
 HOME_DIR = Path(os.path.expanduser("~"))
-PYPERF_BUCKET_DIR = HOME_DIR / config["pyperf_bucket_dir"]
+PYPERF_BUCKET_DIR = HOME_DIR / "buckets" / "pyperf_bucket"
 
 ANALYSIS_DIR = PYPERF_BUCKET_DIR / "analysis"
 ANALYSIS_REPOS_DIR = ANALYSIS_DIR / "repos"
