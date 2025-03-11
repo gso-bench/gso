409848b89f7a6dbdb835eed553db355ac96f857c
manishshettym <manishs@berkeley.edu>
2025-03-11 06:59:50 +0000

    enh(dockerfile): add BLAS/LAPACK and image processing libraries

--- src/pyperf/harness/environment/dockerfile.py
+++ src/pyperf/harness/environment/dockerfile.py
@@ -18,6 +18,28 @@ curl \
 locales \
 locales-all \
 tzdata \
+# Add packages BLAS/LAPACK support
+gcc \
+g++ \
+gfortran \
+libopenblas-dev \
+liblapack-dev \
+pkg-config \
+# Add image processing libs
+libtiff5-dev \
+libjpeg8-dev \
+libopenjp2-7-dev \
+zlib1g-dev \
+libfreetype6-dev \
+liblcms2-dev \
+libwebp-dev \
+tcl8.6-dev \
+tk8.6-dev \
+python3-tk \
+libharfbuzz-dev \
+libfribidi-dev \
+libxcb1-dev \
+libx11-dev \
 && rm -rf /var/lib/apt/lists/*
 
 # Download and install conda
