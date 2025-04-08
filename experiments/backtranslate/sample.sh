b133755b5b28765c71993d07681ea7f4ef2c5950
manishshettym <manishs@berkeley.edu>
2025-04-08 03:03:57 +0000

    refactor: remove unused prepare script and update sample plans script

--- experiments/backtranslate/sample.sh
+++ experiments/backtranslate/sample.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # set -e
 
-echo "========= Preparing dataset for backtranslation ========="
-echo ""
-uv run experiments/backtranslate/prepare_dataset.py --exp_id oversample --push_to_hf --hf_username manishs --dataset_name pyperf-extended
-echo "==============================================================="
-
-
 echo "========= Sampling plans for backtranslation ========="
 echo ""
 uv run experiments/backtranslate/sample_plans.py --model-name o3-mini --max_tokens 24000 --n 5 --use_cache False --temperature 0.5 --multiprocess 50
