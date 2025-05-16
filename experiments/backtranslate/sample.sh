c0d1128782953b6608f8af920d418127ce021e7d
manishshettym <manishs@berkeley.edu>
2025-05-16 11:41:13 -0700

    refactor: remove notebooks etc (maintain in branch)

--- experiments/backtranslate/sample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# set -e
-
-echo "========= Sampling plans for backtranslation ========="
-echo ""
-uv run experiments/backtranslate/sample_plans.py --model-name o4-mini --max_tokens 24000 --n 5 --use_cache False --temperature 0.5 --multiprocess 50
-echo "==============================================================="
